2a0fc67231f94818faa1eba7b9e91c934bfb4739
cjfrew <cjfrew11@gmail.com>
2022-10-11 20:58:41 +1000

    Motion planning and reorganising
    - Started a bit more structure in terms of folders and location of files.
    - Moved everything into src file and then into src/kinematics.
    ----------------------------------------
    - then made the "prototype" / psudocode implementation for motion planning (motionPlanning.py)
    - looks at multiple tags and currently only attempts to go for the closest block.
    - Also checks whether a block is reachable!

--- rot_mtx.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import skw_mtx as sk
-
-def rot_mtx(w,theta):
-    # Rotation matrix: Rodriguez Formula
-    # Exponential e^[w]*theta
-    R = np.identity(3)+np.sin(theta)*sk.skw_mtx(w)+(1-np.cos(theta))*np.linalg.matrix_power(sk.skw_mtx(w), 2)
-    return R
