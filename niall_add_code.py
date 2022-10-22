00f69fbd17a065fb84af32077dbdbc90cb91a9bb
PhucEntropy <niall.lewis.waller@gmail.com>
2022-10-22 16:58:14 +1000

    reached point checker

--- niall_add_code.py
+++ niall_add_code.py
@@ -1,5 +1,5 @@
-from turtle import color
-
+import numpy as np
+import modern_robotics as mr
 
 class GetColor(sm.State):
     def __init__(self):
@@ -27,4 +27,28 @@ class GetColor(sm.State):
                             'GREEN':'green',
                             'YELLOW':'yellow'}
         return color_output_map[self.block_color]
+
+get_color_angles = [0, 0.5, 0, 0] # 30 degree EE
+get_color_angles = [0, 0.715, 0, -0.715] # 41.5 degree EE
+
+L1 = 55     #54
+L2 = 117    #116
+L3 = 95     #94
+L4 = 95     #96
+L5 = 15     #15
+
+CAM_Y = 117
+
+while not reached_point(x, y, z):
+    rp.sleep(0.1)
+
+def reached_point(current_angles, goal_position):
+    current_position = mr.FKinSpace(current_angles)
+
+    for curr_dim, goal_dim in current_position, goal_position:
         
+        if np.abs(curr_dim) < np.abs(goal_dim) - 5 \
+        or np.abs(curr_dim) > np.abs(goal_dim) + 5:
+            return False
+    
+    return True
